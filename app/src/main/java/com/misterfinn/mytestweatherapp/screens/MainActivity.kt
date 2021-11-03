1d7638c8857ce9aab2e46e5422c8fd8ec848d888
mister-finn <misterfinn@tut.by>
2021-11-03 17:26:52 +0300

    Added View Binding

--- app/src/main/java/com/misterfinn/mytestweatherapp/screens/MainActivity.kt
+++ app/src/main/java/com/misterfinn/mytestweatherapp/screens/MainActivity.kt
@@ -3,20 +3,23 @@ package com.misterfinn.mytestweatherapp.screens
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.misterfinn.mytestweatherapp.R
+import com.misterfinn.mytestweatherapp.databinding.ActivityMainBinding
 import com.misterfinn.mytestweatherapp.pojo.ListInfo
 import com.misterfinn.mytestweatherapp.presenter.MainContract
 import com.misterfinn.mytestweatherapp.presenter.MainPresenter
 
 class MainActivity : AppCompatActivity(), MainContract.View {
     private lateinit var presenter:MainPresenter
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         presenter = MainPresenter(this)
     }
 
     override fun displayInfo(weatherList: List<ListInfo>) {
-        
+
     }
 
     override fun onDestroy() {
