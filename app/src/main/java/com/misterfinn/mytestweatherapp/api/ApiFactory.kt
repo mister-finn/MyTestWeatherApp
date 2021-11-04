eb70cca25dd90e0e014fc03adf300c2fd576fb8b
mister-finn <misterfinn@tut.by>
2021-11-04 13:19:43 +0300

    Fixed BASE_URL

--- app/src/main/java/com/misterfinn/mytestweatherapp/api/ApiFactory.kt
+++ app/src/main/java/com/misterfinn/mytestweatherapp/api/ApiFactory.kt
@@ -5,12 +5,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiFactory {
-    private const val BASE_URL = "api.openweathermap.org/data/2.5/"
+    private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
         .baseUrl(BASE_URL)
         .build()
 
-    val apiService = retrofit.create(ApiService::class.java)
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
 }
